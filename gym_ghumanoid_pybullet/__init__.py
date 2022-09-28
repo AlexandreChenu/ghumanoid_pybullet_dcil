9a03c313dfee1c6b58ab72ef3c51598e7c287338
Alex Dell ISIR <alexandre.chenu@ensta-bretagne.org>
2022-09-28 21:39:22 +0100

    add backflip env

--- gym_ghumanoid_pybullet/__init__.py
+++ gym_ghumanoid_pybullet/__init__.py
@@ -25,6 +25,19 @@ register(
     entry_point="gym_ghumanoid_pybullet.envs:GHumanoidGoal",
 )
 
+print("|    gym version and path:", gym.__version__, gym.__path__)
+
+print("|    REGISTERING GHumanoid_BF-v0 from", envpath())
+register(
+    id="GHumanoid_PB_BF-v0",
+    entry_point="gym_ghumanoid_pybullet.envs:GHumanoid_BF",
+)
+
+print("|    REGISTERING GHumanoidGoal_BF-v0 from", envpath())
+register(
+    id="GHumanoidGoal_PB_BF-v0",
+    entry_point="gym_ghumanoid_pybullet.envs:GHumanoidGoal_BF",
+)
 # print("|    REGISTERING GFetchDCIL-v0 from", envpath())
 # register(
 #     id="GFetchDCIL-v0",
--- gym_ghumanoid_pybullet/envs/__init__.py
+++ gym_ghumanoid_pybullet/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_ghumanoid_pybullet.envs.humanoidenv.humanoid_DCIL import GHumanoid, GHumanoidGoal#, GFetchDCIL
+from gym_ghumanoid_pybullet.envs.humanoidenv.humanoid_DCIL_backflip import GHumanoid_BF, GHumanoidGoal_BF#, GFetchDCIL
